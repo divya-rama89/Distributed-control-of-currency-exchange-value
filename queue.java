2a9b5d98bb07278a797439893eee0384681a400c
divya-rama89 <divya.rama89@gmail.com>
2015-02-18 14:27:37 -0500

    Create queue.java

---
+++ queue.java
@@ -0,0 +1,18 @@
+
+public class queue {
+
+	 int delx; 
+	   int dely;
+	   int id; //SenderID
+	   int uID; //OperationID
+	   double clkcount;
+	
+	   public queue(int dx, int dy, int sid, int uid, double clkcount2)
+			   {
+				   delx = dx;
+				   dely = dy;
+				   id = sid;
+				   uID = uid;
+				   clkcount = clkcount2; 
+			   }
+		}
