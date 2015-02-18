5a0b7a6861269081457eb35e7f3097f92a6c5811
divya-rama89 <divya.rama89@gmail.com>
2015-02-18 14:28:08 -0500

    Create randomQ.java

---
+++ randomQ.java
@@ -0,0 +1,14 @@
+import java.io.Serializable;
+
+public class randomQ implements Serializable{
+
+   final int x;
+   final int y; 
+	
+	public randomQ(int a, int b) {
+      this.x = a;
+      this.y = b;
+     // System.out.println("entered here"+ this.x+" "+y);
+	}
+
+}
